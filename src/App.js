9ecc3ea7e1491d450fbec1b3982344f4133450c2
RinuZz <rinujasi32@gmail.com>
2024-02-09 14:13:51 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './Components/Home/Header/Header';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import Home from './Components/pages/HomePages/Home';
 import Service from './Components/Services/Service';
-import Plans from './Components/Plans/Plans';
 import Contact from './Components/Contact/Contact';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Header />
         <Home />
         <Service />
-        <Plans />
         <Contact />
         <Switch>
           <Router path='/' exact component={Home}/>
