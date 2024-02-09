a7a560cb70a6f0746b0d0617cfb991c48279bbf7
RinuZz <rinujasi32@gmail.com>
2024-02-09 14:50:28 +0530

    With-changes

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
         <div className="container flex">
             <div className="right-topMargin">
                 <h1 style={{'font-size': '55px', 'margin-top': '-126px','margin-left': '-227px'}}>Transforming Ideas into Reality.</h1>
-                <p style={{marginLeft:'-227px'}}>Ollinwon focus to have satisfied customers by providing the best service that we can....</p>
+                <p style={{marginLeft:'-227px'}}>Ollinwon focus to have satisfied customers by providing the best service that we can......</p>
                 <a href="contactUs"><button className='primary-btn' style={{cursor:'pointer'}}>Contact Us</button></a>
             </div>
         </div>
